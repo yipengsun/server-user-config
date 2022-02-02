1a2489c48e9fcadaf56061731346c9bcd1c53e6d
Yipeng Sun <syp@umd.edu>
2022-02-02 18:49:00 -0500

    Profile for root user by default.

--- flake.nix
+++ flake.nix
@@ -20,8 +20,8 @@
           stateVersion = "21.11"; # typically you don't change this
 
           # change these yourself
-          homeDirectory = "/home/physicist";
-          username = "physicist";
+          homeDirectory = "/root";
+          username = "root";
 
           configuration = { pkgs, lib, ... }: {
             nixpkgs.overlays = [ overlay ];
