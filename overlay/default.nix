4165606d20b36bd0743e10d3de4a472ce9dcda64
Yipeng Sun <syp@umd.edu>
2023-03-29 09:00:04 -0400

    Update helper scripts.

--- overlay/default.nix
+++ overlay/default.nix
@@ -2,7 +2,7 @@ final: prev:
 {
   # put new packages/package overrides here
   home-build = prev.writeScriptBin "home-build" ''
-    nix build ".#users.physicist.activationPackage"
+    nix build ".#homeConfiguration.physicist.activationPackage"
   '';
   home-switch = prev.writeScriptBin "home-switch" ''
     ${final.home-build}/bin/home-build && ./result/activate
