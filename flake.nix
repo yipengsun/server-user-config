025f8c3d15c24482ad2524eb75f165fb4e3db0f5
Yipeng Sun <syp@umd.edu>
2023-03-29 08:30:49 -0400

    Try to rewrite the config in a sane way.

--- flake.nix
+++ flake.nix
@@ -9,74 +9,9 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils }:
-    let
-      overlay = import ./overlay/default.nix;
-    in
-    {
-      overlay = import ./overlay/default.nix;
-      users = {
-        physicist = home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          stateVersion = "21.11"; # typically you don't change this
-
-          # change these yourself
-          homeDirectory = "/home/syp";
-          username = "syp";
-
-          configuration = { pkgs, lib, ... }: {
-            nixpkgs.overlays = [ overlay ];
-
-            home.packages = with pkgs; [
-              pkgs.home-manager
-
-              # basic utilities
-              bottom # bottom is top
-              ripgrep
-              fd
-              tree
-              git-annex
-
-              # terminal debugging
-              colortest
-
-              # HEP
-              root
-              sxiv # image viewer
-              zathura # pdf viewer
-
-              # dev
-              nixpkgs-review
-              black
-
-              # define additional packages here
-              # you can search the package names on https://search.nixos.org/packages
-            ];
-
-            imports = [
-              # basic tools everyone should want
-              ./profiles/git
-              ./profiles/python
-              ./profiles/tmux # screen replacement
-              ./profiles/fzf # fuzzy find
-              ./profiles/ranger # a nice CLI-based file manager
-
-              # shell
-              ./profiles/zsh
-
-              # dev
-              ./profiles/direnv
-              ./profiles/bat # cat with syntax highlighting
-              ./profiles/neovim # vi
-
-              # config
-              ./profiles/dircolors
-            ];
-          };
-        };
-      };
-    } //
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlay = import ./overlay/default.nix;
         pkgs = import nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
@@ -90,6 +25,66 @@
             home-build
             home-switch
           ];
+        };
+
+        users = {
+          physicist = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+
+            stateVersion = "21.11"; # typically you don't change this
+
+            # change these yourself
+            homeDirectory = "/home/syp";
+            username = "syp";
+
+            configuration = { pkgs, lib, ... }: {
+              home.packages = with pkgs; [
+                pkgs.home-manager
+
+                # basic utilities
+                bottom # bottom is top
+                ripgrep
+                fd
+                tree
+                git-annex
+
+                # terminal debugging
+                colortest
+
+                # HEP
+                root
+                sxiv # image viewer
+                zathura # pdf viewer
+
+                # dev
+                nixpkgs-review
+                black
+
+                # define additional packages here
+                # you can search the package names on https://search.nixos.org/packages
+              ];
+
+              imports = [
+                # basic tools everyone should want
+                ./profiles/git
+                ./profiles/python
+                ./profiles/tmux # screen replacement
+                ./profiles/fzf # fuzzy find
+                ./profiles/ranger # a nice CLI-based file manager
+
+                # shell
+                ./profiles/zsh
+
+                # dev
+                ./profiles/direnv
+                ./profiles/bat # cat with syntax highlighting
+                ./profiles/neovim # vi
+
+                # config
+                ./profiles/dircolors
+              ];
+            };
+          };
         };
       });
 }
